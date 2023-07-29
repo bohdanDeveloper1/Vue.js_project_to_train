de0c1459ac8f6418fee021a908f025ebea5786ff
Bohdan Boboshko <bogdanbobosko0@gmail.com>
2023-07-29 12:38:31 +0300

    Train project Vue.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// відповідає за тип зборки (development or production)
 Vue.config.productionTip = false
 
+ // монтує Vue до елементу із id app in HTML
 new Vue({
   render: h => h(App),
 }).$mount('#app')
